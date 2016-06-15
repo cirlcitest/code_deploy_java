5e1c22ef17b962c6977ce9aebe2afd5d1318c235
testuser <testmail>
2016-06-15 17:20:32 +0530

    file added

---
+++ webservicetest/src/main/java/mypack/Student.java
@@ -0,0 +1,29 @@
+package mypack;
+import javax.xml.bind.annotation.XmlRootElement;
+@XmlRootElement
+public class Student {
+	String type;
+	String name;
+	int age;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	
+	
+}
---
+++ webservicetest/src/main/java/webservicetest/webservice.java
@@ -0,0 +1,5 @@
+package webservicetest;
+
+public class webservice {
+
+}
