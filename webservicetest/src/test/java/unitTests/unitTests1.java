ee761720f141ab59c9e2c2d3564eedb07490a147
testuser <testmail>
2016-06-17 11:37:41 +0530

    file modified

---
+++ webservicetest/src/test/java/unitTests/unitTests1.java
@@ -0,0 +1,21 @@
+package unitTests;
+import org.junit.Test;
+import org.junit.Assert;
+public class unitTests1 {
+
+	@Test
+	public void test1(){
+		
+		Assert.assertEquals(5,5);
+		
+	}
+	
+	@Test
+	public void test2(){
+		
+		Assert.assertEquals(true, false);
+		
+	}
+	
+	
+}
